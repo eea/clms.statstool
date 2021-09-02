db2e4922b4091d866802407158b533e3faf0e8c3
Mikel Larreategi <mlarreategi@codesyntax.com>
2021-09-02 10:27:38 +0200

    pep8/lint/black

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import os
 from os.path import join
 from setuptools import setup, find_packages
 
-NAME = 'clms.statstool'
-PATH = NAME.split('.') + ['version.txt']
+NAME = "clms.statstool"
+PATH = NAME.split(".") + ["version.txt"]
 VERSION = open(join(*PATH)).read().strip()
 
 setup(
@@ -14,8 +14,9 @@ setup(
     description="An add-on for Plone to save download stats of the CLMS datasets",
     long_description_content_type="text/x-rst",
     long_description=(
-        open("README.rst").read() + "\n" +
-        open(os.path.join("docs", "HISTORY.txt")).read()
+        open("README.rst").read()
+        + "\n"
+        + open(os.path.join("docs", "HISTORY.txt")).read()
     ),
     classifiers=[
         "Environment :: Web Environment",
@@ -28,27 +29,27 @@ setup(
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
-    keywords='EEA Add-ons Plone Zope',
-    author='Mikel Larreategi',
-    author_email='mlarreategi@codesyntax.com',
-    url='https://github.com/eea/clms.statstool',
-    license='GPL version 2',
-    packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['clms'],
+    keywords="EEA Add-ons Plone Zope",
+    author="Mikel Larreategi",
+    author_email="mlarreategi@codesyntax.com",
+    url="https://github.com/eea/clms.statstool",
+    license="GPL version 2",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["clms"],
     include_package_data=True,
     zip_safe=False,
-        python_requires="==2.7, >=3.6",
-        install_requires=[
-        'setuptools',
+    python_requires="==2.7, >=3.6",
+    install_requires=[
+        "setuptools",
         # -*- Extra requirements: -*-
     ],
     extras_require={
-        'test': [
-            'plone.app.testing',
+        "test": [
+            "plone.app.testing",
         ],
     },
     entry_points="""
     [z3c.autoinclude.plugin]
     target = plone
-    """
+    """,
 )
